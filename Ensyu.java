5c18c842b7c2306f55e3216e94f54adb104b254a
lcr064 <lcr064@lcr064noMacBook-Air.local>
2023-01-13 11:57:14 +0900

    自販機の成果物です。数箇所回答を参考に制作しました。使用可能な金額の投入ができない状態になりました。それ以外のエラーメッセージ等は実行できました。

--- Ensyu.java
+++ Ensyu.java
@@ -3,25 +3,30 @@ public class Ensyu {
 
 	public static void main(String[] args) {
 
-		int coinA = 0;		//使用可能なコイン
-		int coinB = 0;		//使用不可のコイン
-		int coinOut = 0;	//不適切な数値
+		int coinA = args.length;		//使用可能なコイン
+		/*　args.lengthは値として入れるのでしょうか？*/
+		int coinChe = 0;	//確認された金額
 		int coinSum = 0;    //合計金額
 
 
 
 		for( int i = 0; i < coinA ; i ++ ) {
-			coinA = Integer.parseInt(args[i]);
+			//文字を数値に変換する
+			coinChe = Integer.parseInt(args[i]);
+			//確認されたコインを分岐する。
+					//正常なコインの場合
 			if( coinA == 10 || coinA == 50 || coinA == 100 || coinA == 500) {
-				coinSum += coinA ;
+				//合計金額の上書き
+				coinSum = coinSum + coinChe ;
+							//↑今あるコインの合計
 
-			}else if(coinB == 1 || coinB == 5 ){		//使用不可の場合
-				System.out.println("警告:"+ coinB  + "円玉は使えません。");
+			}else if(coinA == 1 || coinA == 5 ){//使用不可のコインの場合
+				System.out.println("警告:"+ coinChe  + "円玉は使えません。");
 
-			}else { //それ以外として考えると
-				System.out.println("警告:"+ coinOut + "は硬貨として適切ではありません。");
+			}else { //それ以外の不適切な数値の場合
+				System.out.println("警告:"+ coinChe+ "は硬貨として適切ではありません。");
 
-		}
+			}
 		System.out.println("ただ今の合計金額は"+ coinSum  + "円です。");
 
 
