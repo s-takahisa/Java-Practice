27577936d856921ca24aa752c70828f03ce8e10c
s-takahisa <takahisa.suzuki.lincraft.co.jp>
2023-01-13 16:34:12 +0900

    2023/01/13 成果物提出

--- Ensyu_2.java
+++ Ensyu_2.java
@@ -1,75 +1,74 @@
-
 public class Ensyu_2 {
 
 	public static void main(String[] args) {
 
+//定数
+		final String CAKE_SH= "ショートケーキ";
+		final int PRICE_CAKE_SH = 320 ;
+		final String CAKE_MN= "モンブラン";
+		final int PRICE_CAKE_MN = 350;
+		final String CAKE_CHO = "チョコレートケーキ";
+		final int PRICE_CAKE_CHO = 370 ;
+		final String CAKE_TL = "いちごのタルト";
+		final int PRICE_CAKE_TL = 400 ;
+		final String CAKE_CHE= "チーズケーキ";
+		final int PRICE_CAKE_CHE = 300 ;
 
-		final String cake_Sh = "ショートケーキ";
-		final int Price_cake_Sh = 320 ;
-		final String cake_mn= "モンブラン";
-		final int price_cake_mn = 350;
-		final String cake_cho = "チョコレートケーキ";
-		final int price_cake_cho = 370 ;
-		final String cake_tl = "いちごのタルト";
-		final int price_cake_tl = 400 ;
-		final String cake_che= "チーズケーキ";
-		final int price_cake_che = 300 ;
+		String cakeName = "";
+		//ケーキの名前
+		int cakeCount = 0 ;
+		//ケーキの個数
+		int totalPrice = 0;
+		//ケーキの合計金額
+		int payment = 0 ;
+		//支払い金額
+		int checkCakePrice  = 0;
+		//処理対象のケーキの値段
 
-		int cake = 0 ;					//ケーキ
-		int cakeCount = 0 ;				//ケーキの個数
-		int totalPrice = 0;				//ケーキの合計金額
-		int payment = 0 ; 				//支払い金額
-		String cakeName = "";			//ケーキの名前
+		int discountApplying = 1000	;
+		//適用される割引
+		double discount = 0.8;
+		//割引
+		double tax = 1.08;
+		//消費税
 
-		int discountApplying = 1000;    //適用される割引
-		double discount = 0.2;  		//割引
-		double tax = 1.08;				//消費税
-
-
-
-		for(int i = 0; i < args.length ; i ++ ) {
-			if( 1 % 2 == 0 ) {
+		for (int i = 0 ; i < args.length ; i++) {
+			if(i % 2 == 0) {
 				cakeName  = args[i];
 				switch(cakeName) {
-					case cake_Sh :
-						cakeCount = Price_cake_Sh ;
+					case CAKE_SH :
+						checkCakePrice = PRICE_CAKE_SH ;
 						break;
 
-					case cake_mn :
-						cakeCount = price_cake_mn ;
+					case CAKE_MN :
+						checkCakePrice = PRICE_CAKE_MN ;
 						break;
 
-					case cake_cho :
-						cakeCount = price_cake_cho ;
+					case CAKE_CHO :
+						checkCakePrice = PRICE_CAKE_CHO ;
 						break;
 
-					case cake_tl :
-						cakeCount = price_cake_cho ;
+					case CAKE_TL :
+						checkCakePrice = PRICE_CAKE_TL ;
 						break;
 
-					case cake_che :
-						cakeCount = price_cake_cho ;
+					case CAKE_CHE :
+						checkCakePrice = PRICE_CAKE_CHE ;
 						break;
 				}
 
-			}else {
+			}else{
+				  cakeCount = Integer.parseInt(args[i]);
+				  totalPrice += checkCakePrice * cakeCount ;
 
-				cakeCount = Integer.parseInt( args[i] ) ;
-				totalPrice += cake * cakeCount ;
+				  if(totalPrice > discountApplying) {
+					payment = (int) (totalPrice * tax * discount) ;
+			 	  }else{
+					payment = (int) (totalPrice * tax);
+				  }
+				  System.out.println("合計金額は" + payment + "円です。");
 			}
-			if(totalPrice > discountApplying) {
-				payment = (int) (totalPrice * tax * discount) ;
-
-			}else {
-				payment = (int) (totalPrice * tax);
-			}
-
-
-
-
 		}
-
-		System.out.println("合計金額は" + payment + "円です。");
 	}
-
 }
+
