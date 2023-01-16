fc7d716dead1f4401cf0d375a7d549b7bdd1d491
s-takahisa <takahisa.suzuki.lincraft.co.jp>
2023-01-16 10:53:06 +0900

    再提出

--- 提出成果物/Ensyu.java
+++ 提出成果物/Ensyu.java
@@ -4,27 +4,27 @@ public class Ensyu {
 		//使用可能なコイン
 		int coinUse = args.length;
 		//確認された金額
-		int coinChe = 0;
+		int coinCheck = 0;
 		//合計金額
 		int coinSum = 0;
 		//文字を数値に変換する
-		for( int i = 0; i < coinUse ; i++ ) {
+		for(int i = 0; i < coinUse ; i++) {
 			//確認されたコインを分岐する。
-			coinChe = Integer.parseInt(args[i]);
+			coinCheck = Integer.parseInt(args[i]);
 			//正常なコインの場合
-			if( coinChe == 10 || coinChe == 50 || coinChe == 100 || coinChe == 500) {
+			if(coinCheck == 10 || coinCheck == 50 || coinCheck == 100 || coinCheck == 500) {
 				//合計金額の上書き
-				coinSum += coinChe ;
+				coinSum += coinCheck;
 
 			//使用不可のコインの場合
-			}else if(coinChe == 1 || coinChe == 5 ){
-				System.out.println("警告:"+ coinChe  + "円玉は使えません。");
+			}else if(coinCheck == 1 || coinCheck == 5){
+				System.out.println("警告:"+ coinCheck  + "円玉は使えません。");
 			//それ以外の不適切な数値の場合
-			}else {
-				System.out.println("警告:"+ coinChe+ "は硬貨として適切ではありません。");
+			}else{
+				System.out.println("警告:"+ coinCheck+ "は硬貨として適切ではありません。");
 			}
 		}
-				System.out.println("ただ今の合計金額は"+ coinSum  + "円です。");
+				System.out.println("ただ今の合計金額は"+ coinSum + "円です。");
 	}
 
 }
--- 提出成果物/Ensyu_2.java
+++ 提出成果物/Ensyu_2.java
@@ -4,68 +4,68 @@ public class Ensyu_2 {
 
 		//定数
 		final String CAKE_SH= "ショートケーキ";
-		final int PRICE_CAKE_SH = 320 ;
+		final int PRICE_CAKE_SH = 320;
 		final String CAKE_MN= "モンブラン";
 		final int PRICE_CAKE_MN = 350;
 		final String CAKE_CHO = "チョコレートケーキ";
-		final int PRICE_CAKE_CHO = 370 ;
+		final int PRICE_CAKE_CHO = 370;
 		final String CAKE_TL = "いちごのタルト";
-		final int PRICE_CAKE_TL = 400 ;
+		final int PRICE_CAKE_TL = 400;
 		final String CAKE_CHE= "チーズケーキ";
-		final int PRICE_CAKE_CHE = 300 ;
+		final int PRICE_CAKE_CHE = 300;
 
 		//ケーキの名前
 		String cakeName = "";
 		//ケーキの個数
-		int cakeCount = 0 ;
+		int cakeCount = 0;
 		//ケーキの合計金額
 		int totalPrice = 0;
 		//支払い金額
-		int payment = 0 ;
+		int payment = 0;
 		//処理対象のケーキの値段
 		int checkCakePrice  = 0;
 
 		//適用される割引
-		final int discountApplying = 1000;
+		final int DISCOUNT_APPLYING = 1000;
 		//割引
-		final double discount = 0.8;
+		final double DISCOUNT = 0.8;
 		//消費税
-		final double tax = 1.08;
+		final double TAX = 1.08;
 
 
 		for (int i = 0 ; i < args.length ; i++) {
-			if(i % 2 == 0) {
+			if(i % 2 == 0){
 				cakeName  = args[i];
-				switch(cakeName) {
-					case CAKE_SH :
-						checkCakePrice = PRICE_CAKE_SH ;
+				switch(cakeName){
+					case CAKE_SH:
+						checkCakePrice = PRICE_CAKE_SH;
 						break;
 
-					case CAKE_MN :
-						checkCakePrice = PRICE_CAKE_MN ;
+					case CAKE_MN:
+						checkCakePrice = PRICE_CAKE_MN;
 						break;
 
-					case CAKE_CHO :
-						checkCakePrice = PRICE_CAKE_CHO ;
+					case CAKE_CHO:
+						checkCakePrice = PRICE_CAKE_CHO;
 						break;
 
-					case CAKE_TL :
-						checkCakePrice = PRICE_CAKE_TL ;
+					case CAKE_TL:
+						checkCakePrice = PRICE_CAKE_TL;
 						break;
 
-					case CAKE_CHE :
-						checkCakePrice = PRICE_CAKE_CHE ;
+					case CAKE_CHE:
+						checkCakePrice = PRICE_CAKE_CHE;
 						break;
 				}
 
 			}else{
 				  cakeCount = Integer.parseInt(args[i]);
-				  totalPrice += checkCakePrice * cakeCount ;
+				  totalPrice += checkCakePrice * cakeCount;
 
-				  if(totalPrice > discountApplying) {
-					payment = (int) (totalPrice * tax * discount) ;
+				  if(totalPrice > DISCOUNT_APPLYING){
+					payment = (int) (totalPrice * TAX * DISCOUNT);
 			 	  }else{
-					payment = (int) (totalPrice * tax);
+					payment = (int) (totalPrice * TAX);
 				  }
 				  System.out.println("合計金額は" + payment + "円です。");
 			}
